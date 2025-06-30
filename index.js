037ceec1b7c7a25554a5927323b43cd752b9e0f2
Vlad <montenwav@gmail.com>
2025-06-30 21:54:18 +0300

    static fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
  res.sendFile(path.resolve(__dirname, './public/html/index.html'));
