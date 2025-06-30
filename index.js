9a1dd58ae8edc9749ae2b24f6139c5dfd7932756
Vlad <montenwav@gmail.com>
2025-06-30 22:32:55 +0300

    commit

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('public/images'));
+app.use(express.static('public/css'));
+app.use(express.static('public/js'));
+app.use(express.static('public/html'));
 
 app.get('/', (req, res) => {
  res.sendFile(path.resolve(__dirname, './public/html/index.html'));
