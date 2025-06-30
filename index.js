831ef76cdf5527d00fc9a7c5c36afaca53409696
Vlad <montenwav@gmail.com>
2025-06-30 21:47:49 +0300

    vercel.json fixes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
- res.sendFile(path.resolve(__dirname, '../public/html/index.html')); 
+ res.sendFile(path.resolve(__dirname, './public/html/index.html'));
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
